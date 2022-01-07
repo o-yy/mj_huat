28cbf2990232824fa03c266581df829aafe91a1a
yueyang.ong@wright.com.sg <yueyang.ong@wright.com.sg>
2022-01-07 17:35:24 +0800

    WIP UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 // Copyright 2018 The Flutter team. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 import 'dart:math';
@@ -39,7 +36,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _dice = 0;
-  var winds = {1:"东", 2:"南", 3: "西", 4: "北"};
+  var winds = {1: "东", 2: "南", 3: "西", 4: "北"};
   int counter = 0;
 
   void _rollDice(int id) {
@@ -81,124 +78,123 @@ class _MyHomePageState extends State<MyHomePage> {
                 icon: const Icon(Icons.list))
           ],
         ),
-        body: Column(children: <Widget>[
-          Row(
-            //ROW 1
-            children: [
-              Container(
-                color: Colors.orange,
-                margin: EdgeInsets.all(25.0),
-                child: FlutterLogo(
-                  size: 60.0,
-                ),
-              ),
-              Expanded(
-                child: Transform.rotate(
-                  angle: math.pi,
-                  child: TextButton(
-                    onPressed: () => {
-                      _rollDice(2)
-                    },
-                    child: Card(
-                      child: Text(
-                      (winds[2]).toString(),
+        body: Container(
+          color: Colors.grey,
+          child: Center(
+            child: Container(
+              color: Colors.green,
+              child: AspectRatio(
+                aspectRatio: 1.0,
+                child: Column(children: <Widget>[
+                  Row(
+                    //ROW 1
+                    children: [
+                      Container(
+                        child: SizedBox(),
                       ),
-                    color: counter == 2 ? Colors.red : Colors.transparent,
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                color: Colors.purple,
-                margin: EdgeInsets.all(25.0),
-                child: FlutterLogo(
-                  size: 60.0,
-                ),
-              ),
-            ],
-          ),
-          Expanded(
-            //ROW 2
-            child: Row(children: [
-              Expanded(
-                child: Transform.rotate(
-                  angle: math.pi / 2,
-                  child: TextButton(
-                    onPressed: () => {
-                      _rollDice(3)
-                    },
-                    child: Card(
-                      child: Text(
-                        (winds[3]).toString(),
+                      Expanded(
+                        child: Transform.rotate(
+                          angle: math.pi,
+                          child: TextButton(
+                            onPressed: () => {_rollDice(2)},
+                            child: Card(
+                              child: Text(
+                                (winds[2]).toString(),
+                                style: TextStyle(fontSize: 90),
+                              ),
+                              color:
+                                  counter == 2 ? Colors.red : Colors.grey[300],
+                            ),
+                          ),
+                        ),
                       ),
-                      color: counter == 3 ? Colors.red : Colors.transparent,
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                  color: Colors.blue,
-                  margin: EdgeInsets.all(25.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      const Text(
-                        'You rolled a:',
-                      ),
-                      Text(
-                        '$_dice',
-                        style: Theme.of(context).textTheme.headline4,
+                      Container(
+                        child: SizedBox(),
                       ),
                     ],
-                  )),
-              Expanded(
-                child: Transform.rotate(
-                  angle: -math.pi / 2,
-                  child: TextButton(
-                    onPressed: () => {
-                      _rollDice(1)
-                    },
-                    child: Card(
-                      child: Text(
-                        (winds[1]).toString(),
+                  ),
+                  Expanded(
+                    //ROW 2
+                    child: Row(children: [
+                      Expanded(
+                        child: Transform.rotate(
+                          angle: math.pi / 2,
+                          child: TextButton(
+                            onPressed: () => {_rollDice(3)},
+                            child: Card(
+                              child: Text(
+                                (winds[3]).toString(),
+                                style: TextStyle(fontSize: 90),
+                              ),
+                              color:
+                                  counter == 3 ? Colors.red : Colors.grey[300],
+                            ),
+                          ),
+                        ),
                       ),
-                      color: counter == 1 ? Colors.red : Colors.transparent,
-                    ),
+                      Container(
+                          height: 70,
+                          width: 100,
+                          color: Colors.lightGreen,
+                          margin: EdgeInsets.all(25.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              const Text(
+                                'You rolled a:',
+                              ),
+                              Text(
+                                '$_dice',
+                                style: Theme.of(context).textTheme.headline4,
+                              ),
+                            ],
+                          )),
+                      Expanded(
+                        child: Transform.rotate(
+                          angle: -math.pi / 2,
+                          child: TextButton(
+                            onPressed: () => {_rollDice(1)},
+                            child: Card(
+                              child: Text(
+                                (winds[1]).toString(),
+                                style: TextStyle(fontSize: 90),
+                              ),
+                              color:
+                                  counter == 1 ? Colors.red : Colors.grey[300],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ]),
                   ),
-                ),
+                  Row(// ROW 3
+                      children: [
+                    Container(
+                      child: SizedBox(),
+                    ),
+                    Expanded(
+                        child: Container(
+                      color: Colors.green,
+                      child: TextButton(
+                        onPressed: () => {_rollDice(4)},
+                        child: Card(
+                          child: Text(
+                            (winds[4]).toString(),
+                            style: TextStyle(fontSize: 90),
+                          ),
+                          color: counter == 4 ? Colors.red : Colors.grey[300],
+                        ),
+                      ),
+                    )),
+                    Container(
+                      child: SizedBox(),
+                    ),
+                  ]),
+                ]),
               ),
-            ]),
+            ),
           ),
-          Row(// ROW 3
-              children: [
-            Container(
-              color: Colors.orange,
-              margin: EdgeInsets.all(25.0),
-              child: FlutterLogo(
-                size: 60.0,
-              ),
-            ),
-            Expanded(
-                child: TextButton(
-              onPressed: () => {
-                _rollDice(4)
-              },
-                  child: Card(
-                    child: Text(
-                      (winds[4]).toString(),
-                    ),
-                    color: counter == 4 ? Colors.red : Colors.transparent,
-                  ),
-            )),
-            Container(
-              color: Colors.purple,
-              margin: EdgeInsets.all(25.0),
-              child: FlutterLogo(
-                size: 60.0,
-              ),
-            ),
-          ]),
-        ]),
+        ),
       ),
     );
   }
